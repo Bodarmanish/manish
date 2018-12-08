6fc7c6fee102b9465cc9305a6c779403d36921a7
Bodarmanish <bodarmanish777@gmail.com>
2018-12-08 17:48:07 +0530

    table

--- database/migrations/2018_11_24_093729_create_students_table.php
+++ database/migrations/2018_11_24_093729_create_students_table.php
@@ -17,6 +17,7 @@ class CreateStudentsTable extends Migration
             $table->increments('id');
             $table->string("name");
             $table->string("email");
+            $table->string("image");
             $table->timestamp("created_dt");
             //$table->timestamps();
         });
