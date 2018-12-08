563da75c1a43a2fd9db7d748025bd5e1c5e5cd8c
Bodarmanish <bodarmanish777@gmail.com>
2018-12-08 17:25:17 +0530

    my first commitsave changes

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -44,9 +44,7 @@
         <button type="submit" class="btn btn-success"style="margin-top:10px">Submit</button>
     </div>    
 </form> 
-       <!-- <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
-        <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
-        {!! Toastr::message() !!}-->
+       
 @endsection
 
 
